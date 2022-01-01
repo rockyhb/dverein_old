9925b6ecd41eecb2f2d3405ee2902184f0b5b111
Claas Rockmann-Buchterkirche <claas@rockbu.de>
2022-01-01 17:59:52 +0100

    adjusted dockerfile

--- docker-setup/init_and_run.sh
+++ docker-setup/init_and_run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-cd /var/www
+cd /code
 python3 manage.py wait_for_db
 python3 manage.py migrate || { echo 'migrate failed' ; exit 1; }
 python3 manage.py collectstatic --noinput
 python3 manage.py initadmin
 python3 manage.py initial_data
-cd /home/docker
-/usr/local/bin/uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi-app.ini
+python3 manage.py runserver 0.0.0.0:8000
