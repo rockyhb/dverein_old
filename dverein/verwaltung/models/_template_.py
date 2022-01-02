49d1177136fac474d06cb799dec30c575afaa113
Claas Rockmann-Buchterkirche <claas@rockbu.de>
2022-01-02 01:30:52 +0100

    fixed models imports

--- dverein/verwaltung/models/_template_.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.db.models.deletion import CASCADE, SET_NULL
-from django.db.models.expressions import F
-from django.db.models.fields import BinaryField, BooleanField, CharField, DateField, DateTimeField, FloatField, IntegerField, TextField
-from django.db.models.fields.related import ForeignKey
-from django.db.models.lookups import In
-
-class _template_(models.Model):
-    pass
